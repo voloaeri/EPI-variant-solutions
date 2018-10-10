215e40810bb077c6a06fb1a4c63a25f9f7059c20
Volodymyr Siedlecki <volosied@gmail.com>
2018-10-10 11:03:09 -0400

    add correction to mod power 2

--- chapter-4/4-1.py
+++ chapter-4/4-1.py
@@ -5,11 +5,8 @@ def right_propagate(x):
     return x-1 | x
 
 # Compute x mod a power of two  (17 mod 8 = 1)
-# Cannot be tested: INFINITE number of bits are used for Python negative numbers.
-# -5 is treated by bitwise operators as if it were written "...1111111111111111111011".
 def mod_power_2(x,a):
-     a = a << 1
-     return x ^ -a
+     return x & a-1
 
 # Return true if x is a power of 2
 def is_power_of_2(x):
@@ -20,7 +17,7 @@ def is_power_of_2(x):
 
 print("Right progpagation of {} is {}".format(bin(64), bin(right_propagate(64))))
 print("Right progpagation of {} is {}".format(bin(200), bin(right_propagate(200))))
-# print("26 mod 4 is {}".format(mod_power_2(26,4)))
-# print("21 mod 8 is {}".format(mod_power_2(21,8)))
+print("26 mod 4 is {}".format(mod_power_2(26,4)))
+print("21 mod 8 is {}".format(mod_power_2(21,8)))
 print("Is 16 a power of 2: {}".format(is_power_of_2(16)))
 print("Is 30 a power of 2: {}".format(is_power_of_2(30)))
